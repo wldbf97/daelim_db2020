e10d5d779d11249519951c44b988b7d02e6f491e
JiYoul <wldbf97@naver.com>
2020-05-19 11:40:37 +0900

    commit

--- modules/JiYoul/hello.php
+++ modules/JiYoul/hello.php
@@ -3,9 +3,18 @@ namespace Modules\JiYoul;
 
 // 클래스 선언
 class Hello{
+
+    //상수선언
+    const MYNAME = "jiyoul";
+
     //클래스의 함수
     //메소드 함.
     public function greeting(){
-        echo "JiYoul!!!! hello world";
-    } 
+        echo self::MYNAME . "hello world";
+    }
+
+    //생성자
+    public function __construct(){
+        echo __CLASS__ . "객체를 생성합니다. <br>";
+    }
 }
